bfd5a64f956401fbb53cdb13aae9c5f139fcccf7
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-04-04 18:23:23 +0530

    Add files via upload

---
+++ q10.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Question 10</h1>
+        <p>First Name:{{first_name}}</p>
+        <p>Last Name{{last_name}}</p>
+        <p>FullName:{{fullname}}</p>
+        <button @click="namefun()" >clickme</button>
+    </div>
+</template>
+<script>
+    export default{
+        name:"Q10ASS",
+        data(){
+            return{
+                first_name: "500apps" , 
+                last_name :"infinity",
+                fullname:" "
+
+            }
+        },
+        methods:{
+            namefun(){
+                this.fullname=this.first_name+this.last_name
+                console.log(this.fullname);
+            }
+
+        }
+    }
+</script>

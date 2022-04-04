f47fc6af73265c7639085001df533b8fe0203226
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-04-04 18:22:22 +0530

    Add files via upload

---
+++ q7.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <head>Assignment-4</head>
+    <h1>Question -7</h1>
+<button @click="opens()">Opens</button>
+<p>{{opendata}}</p>
+<button @click="pending()">Pending</button>
+<p>{{pendingdata}}</p>
+<p>{{pendingdata1}}</p>
+<button @click="closed()">Closed</button>
+<p>{{closeddata}}</p>
+</div>
+</template>
+<script>
+export default{
+name:"queS7",
+data(){
+return{
+data :[
+{"email": "Eliseo@gardner.biz","id": 1,"name": "id labore ex et quamlaborum","status":"open"},
+{"email": "Jayne_Kuhic@sydney.com","id": 2,"name": "quovero reiciendis velit similiqueearum","status":"pending"},
+{"email": "Nikita@garfield.biz","id": 3,"name":"odioadipisci rerum autanimi","status":"closed"},
+{"email": "Lew@alysha.tv","id": 4,"name": "alias odio sit","status":"pending"}
+],
+opendata:"",
+pendingdata:"",
+pendingdata1:'',
+closeddata:"",
+text:""
+}
+},
+methods:{
+opens(){
+this.opendata=this.data[0];
+},
+pending(){
+this.pendingdata=this.data[1];
+this.pendingdata1=this.data[3];
+},
+closed(){
+this.closeddata=this.data[2];
+}
+}
+}
+</script>
+

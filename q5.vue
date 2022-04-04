57794c1b271b983276e85746ddbe1b1f34dfc5dc
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-04-04 18:20:22 +0530

    Add files via upload

---
+++ q5.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <head>Assignment-4</head>
+        <h1>Question-5</h1>
+        <p>This is errors Handling</p>
+        <button @click="errorshandling()">catch</button>
+    </div>
+</template>
+<script>
+export default{
+    name:"Q5DD",
+    data(){
+        return{
+
+        }
+    },
+methods:{
+errorshandling() {
+try{
+this.test = this.val * 45;
+console.log("test",this.val);
+}
+catch{console.log("Error caught")}
+}
+}
+}
+</script>

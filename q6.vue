2ab9f32c6ef99f1ce703d755dc302e86890d62cb
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-04-04 18:21:53 +0530

    Add files via upload

---
+++ q6.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <h1>Question -6</h1>
+    <p>get the name from parent component</p>
+<p>Name:{{ name }}</p>
+</div>
+</template>
+<script>
+export default {
+    name: "Q6DD",
+data() {
+return {
+name: "Anusha",
+};
+},
+};
+
+</script>

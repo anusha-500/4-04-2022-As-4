6c522d27195702c21ffc63887f74e17a44723b50
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-04-04 18:20:04 +0530

    Add files via upload

---
+++ q4.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <head>Assignment-4</head>
+        <h1>Question 4</h1>
+        <p> fetch the data from the given url</p>
+      
+        </div>
+</template>
+<script>
+//var axios = require("axios").default;
+export default {
+  name: "q4ANS",
+  data() {
+    return {
+        async created() {
+  // GET request using fetch with async/await
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1/comments");
+  const data = await response.json();
+  this.totalVuePackages = data.total;
+}
+    }
+    }
+  }
+
+  </script>
+    

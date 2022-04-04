a3e33e1ea309c663a9490c2b7a67a65ea3ca997c
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-04-04 18:22:43 +0530

    Add files via upload

---
+++ q8.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <h1>Question -8</h1>
+    <p> using p-tag</p>
+<p @click="result()">p-tag</p>
+<textarea id="sample"> </textarea>
+</div>
+</template>
+<script>
+export default {
+name: "QuesT8",
+data() {
+return {
+text: "",
+};
+},
+methods: {
+result() {
+this.text = document.write("" + document.getElementById("sample").value);
+},
+},
+};
+</script>
